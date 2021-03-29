e4d447b83dc8fb8bba00dc3006ab88190a34636b
GreyWay2 <greenpeesoleg@gmail.com>
2021-03-29 17:59:55 +0400

    feat: add dream team and what season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let res = []
+  if (!Array.isArray(members)) {return false}
+  members.forEach(el => {
+    if (typeof el === 'string') {
+      res.push(el.trim().charAt(0).toUpperCase())
+    }
+  });
+  if (!res.length) {return false}
+  return res.sort().join('')
 };
